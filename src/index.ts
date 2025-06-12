a4934839038e3d3cfe6164a9a2eb5ea54771b84d
wesleythorsen1 <wesley.thorsen@gmail.com>
2025-06-11 23:59:08 -0400

    fix: add /console to exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-export * from './core';
+export * from '@bussin/context-logger/core';
 
 // use console implementation by default.
 // can be swapped for other implementations (winston, pino, log4js, console)
 // but would warrant a major version bump due to options model change.
-export * from './console/createLogger';
-export * from './console/logger';
+export { createLogger, logger } from '@bussin/context-logger/console';
 
 // // to use winston implementation:
-// export * from './winston/createLogger';
-// export * from './winston/logger';
+// export { createLogger, logger } from '@bussin/context-logger/winston';
